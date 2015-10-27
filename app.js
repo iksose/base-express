c01ac6daf58f5fde20839cddaffbd325ce740cfc
Jonathan Jennings <iksose.pt3@gmail.com>
2015-10-27 12:16:03 -0500

    first update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require('express')
-  , app = express()
-  , bodyParser = require('body-parser')
-  , port = process.env.PORT || 3000
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.port || 3000;
 
 app.set('views', __dirname + '/views')
 app.engine('jade', require('jade').__express)
@@ -9,7 +10,9 @@ app.set('view engine', 'jade')
 
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(require('./controllers'))
 
 app.listen(port, function() {
